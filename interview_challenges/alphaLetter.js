b8947b748890c26a8bb24f1b94f93ca1c8ccff44
Ryan Udelhoven <rudelhoven@rbh12277.redbrickhealth.local>
2019-01-28 12:07:32 -0600

    cleaned up some not working challenge code

--- interview_challenges/alphaLetter.js
+++ interview_challenges/alphaLetter.js
@@ -1,7 +1,7 @@
 console.log('alphaLetter');
 console.log('Complete');
 
-// Given a string find the character that occurs most often. DONE!
+// Given a string find the character that occurs most often.
 // If two characters are tied for most, choose the one that occurs first.
 
 // == INPUT RULES ==
@@ -11,24 +11,7 @@ console.log('Complete');
 let testWord = 'helloworld';
 // =========================
 
-
-// Solution #1
-function alphaLetter1(input) {
-  var myMap = new Map();
-  input.split('').forEach( item => myMap.has(item) ? myMap.set(item, myMap.get(item) + 1) : myMap.set(item, 1) );
-  let highCount = 0;
-  myMap.forEach( item => highCount < item ? highCount = item : null );
-  let short = myMap.entries().next().value;
-  myMap.forEach((item) => { item == highCount ? result = [short[0], short[1]] : null });
-  return result;
-};
-
-console.log('Solution 1:', alphaLetter1(testWord));
-// Solution #1 does not currently work.
-// The short variable is not cycling to the proper letter, it only takes in the first one.
-
-// Solution #2
-function alphaLetter2(input) {
+function alphaLetter(input) {
   var myMap = new Map();
   input.split('').forEach( key => myMap.has(key) ? myMap.set(key, myMap.get(key) + 1) : myMap.set(key, 1) );
   let highCount = 0;
@@ -40,5 +23,5 @@ function alphaLetter2(input) {
   };
 };
 
-console.log('Solution 2:', alphaLetter2(testWord));
-// This solution works on every test word.
+console.log('Solution 2:', alphaLetter(testWord));
+// This solution works on every test string of any length.
