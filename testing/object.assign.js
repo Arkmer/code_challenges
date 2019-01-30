96b6962a1440ce5c29d64469509d4680b9881e7d
Ryan Udelhoven <rudelhoven@rbh12277.redbrickhealth.local>
2019-01-30 15:44:41 -0600

    poked around with <deep cloning> with Object.assign.

--- testing/object.assign.js
+++ testing/object.assign.js
@@ -21,3 +21,14 @@ console.log('3', obj3);
 let obj4 = Object.assign({c: 'over', d: 4}, obj);
 
 console.log('4', obj4);
+
+objDeep = {
+  z: {
+    deepLevel: '<<Can we see this?>>',
+  }
+}
+
+let obj5 = Object.assign(obj, {c: objDeep, d: 4});
+
+console.log('5: Deep Cloning?', obj5);
+console.log('5: Close up?', obj5.c.z.deepLevel);
