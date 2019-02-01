25b8fc299b5f2e661710108f687aabe29f365c5a
Ryan Udelhoven <rudelhoven@rbh12277.redbrickhealth.local>
2019-02-01 14:42:34 -0600

    high number frequency completed

---
+++ hacker_rank/highNumberFrequency.js
@@ -0,0 +1,33 @@
+console.log('highNumberFrequency');
+
+// Find the number of times the highest number is used in an array.
+
+function arrayLooper(arr){
+  let highNumberFrequency = 0;
+  let max = Math.max(...arr)
+  arr.forEach( num => num == max ? highNumberFrequency++ : null );
+  return highNumberFrequency;
+};
+
+function randomArrayGen(max, length){
+  let testArray = [];
+  for(let i=0; i < length; i++){
+    testArray.push(Math.floor(Math.random() * Math.floor(max)));
+  };
+  console.log(testArray.sort((a, b) => a - b));
+  return testArray;
+};
+
+console.log(arrayLooper(randomArrayGen(3, 100)));
+
+function averageFinder(times){
+  let compiledArray = new Array();
+  for(let i=0; i<times; i++){
+    compiledArray.push(arrayLooper(randomArrayGen(3, 100)))
+  };
+  console.log(compiledArray);
+  return compiledArray.reduce((acc, cur) => acc + cur)/times;
+};
+
+// Comment out some of the console.log()s before using this.
+// console.log(averageFinder(100));
